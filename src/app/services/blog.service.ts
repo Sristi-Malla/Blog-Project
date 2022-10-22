4542da0928d136c465bf1fec5bfa9cbb75d104f8
Sristi <mallasristi6@gmail.com>
2022-10-22 12:32:35 +0530

    added more features

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient,HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { Blog } from '../models/blog.model';
-
+import { catchError } from 'rxjs/operators';
 
 
 @Injectable({
@@ -14,15 +14,21 @@ export class BlogService {
   constructor(private http: HttpClient) { }
 
   getAll(): Observable<Blog[]> {
-    return this.http.get<Blog[]>(this.baseUrl);
+    return this.http.get<Blog[]>(this.baseUrl).pipe(
+      catchError(this.handleError)
+    );
   }
 
   get(id: any): Observable<Blog> {
-    return this.http.get<Blog>(this.baseUrl+"/"+id);
+    return this.http.get<Blog>(this.baseUrl+"/"+id).pipe(
+      catchError(this.handleError)
+    );
   }
 
   create(data: any): Observable<any> {
-    return this.http.post(this.baseUrl, data);
+    return this.http.post(this.baseUrl, data).pipe(
+      catchError(this.handleError)
+    );
   }
 
   update(id: any, data: any): Observable<any> {
@@ -30,7 +36,9 @@ export class BlogService {
   }
 
   delete(id: any): Observable<any> {
-    return this.http.delete(`${this.baseUrl}/${id}`);
+    return this.http.delete(`${this.baseUrl}/${id}`).pipe(
+      catchError(this.handleError)
+    );
   }
 
   deleteAll(): Observable<any> {
@@ -38,6 +46,20 @@ export class BlogService {
   }
 
   findByTitle(title: any): Observable<Blog[]> {
-    return this.http.get<Blog[]>(`${this.baseUrl}?title=${title}`);
+    return this.http.get<Blog[]>(`${this.baseUrl}?title_like=${title}`).pipe(
+      catchError(this.handleError)
+    );
   }
+
+  handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
 }
